2a52b0532bfd46b067fd6a320ad107be6ab4b206
gabiazevedo <gabiazevedoms@gmail.com>
2021-08-22 16:57:46 -0300

    Criaçao de funcionalidades do Projeto LetMeAsk - Aula 2 - Evento NLW

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
+import { ButtonHTMLAttributes } from "react";
+import '../styles/button.scss';
 
-export function Button() {
-  const [ counter, setCounter ] = useState(0);
-  function increment() {
-    setCounter(counter + 1)
-  }
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: ButtonProps) {
   return (
-    <button onClick={increment}>{counter}</button>
+    <button className="button" {...props} />
   );
 }
+
+// Criando botão que será reutilizado em toda aplicação;
+// Recebe por parâmetro na função as props e repassa para o elemento HTML na linha 7,
+// utilizando o ...(spred operator).
+// Para definir as propriedades que serão recebidas utilizamos o HTML Attributes (linha) 1
+// e setamos essa confirguração na linha 3 e passamos todos estes atributos via parâmetro da função. 
